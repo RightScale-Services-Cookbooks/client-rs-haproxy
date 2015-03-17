b3bd10c31d62c7a3ab97b918a50c0393ee0a4698
tusharshahrs <tushar.shah@rightscale.com>
2015-03-17 14:50:36 -0400

    added http-check

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,8 +34,8 @@ updated_haproxy_config = Mash.new(
       # rspidel entries
       'rspidel' => ['^Set-cookie:\ IP=', '^Server:']
       #'http-check expect' => ['status 401|30*|20*']
-      'http-check' => 'expect'=> 'status' =>['401|30*|20*']
-      #"http-check expect status 401|30*|20*"
+      #'http-check' => 'expect'=> 'status' =>['401|30*|20*']
+      "http-check" => ['expect status 401|30*|20*']
     }
   }
 )

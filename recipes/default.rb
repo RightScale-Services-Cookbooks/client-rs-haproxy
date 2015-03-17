b3ddc1ea0a29411e27bba74d053ba3cdd08eddd0
tusharshahrs <tushar.shah@rightscale.com>
2015-03-17 14:57:53 -0400

    added http-check

--- recipes/default.rb
+++ recipes/default.rb
@@ -36,8 +36,8 @@ updated_haproxy_config = Mash.new(
       #'http-check expect' => ['status 401|30*|20*']
       #'http-check' => 'expect'=> 'status' =>['401|30*|20*']
       #"http-check" => ['expect status 401|30*|20*']
-      'http-check expect status'=> '200'
-      #'http-\check expect status'=> '200'
+      #'http-check expect status'=> '200'
+      'http-\check expect status'=> '200'
     }
   }
 )

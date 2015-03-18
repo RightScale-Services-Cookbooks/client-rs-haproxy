1ae10365ba0bc5f7f6b461e54873fef92a4e151a
tusharshahrs <tushar.shah@rightscale.com>
2015-03-18 17:09:47 -0400

    removed rewind

--- recipes/default.rb
+++ recipes/default.rb
@@ -42,7 +42,7 @@ updated_haproxy_config = Mash.new(
 include_recipe 'rs-haproxy::default'
 #include_recipe 'rsc_rs-haproxy::default'
 
-rewind 'haproxy[set up haproxy.cnf]' do
-  config updated_haproxy_config
-  action :create
-end
+#rewind 'haproxy[set up haproxy.cnf]' do
+#  config updated_haproxy_config
+#  action :create
+#end

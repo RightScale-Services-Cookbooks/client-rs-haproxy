e42db45245efb29dc675e68c7d3fc44c2b08761b
Richard Shade <rshade@rightscale.com>
2015-04-03 16:34:20 -0500

    adding timeouts

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,6 @@
-node.default[:haproxy][:config][:defaults]['options'] << 'x_forwarded_for'
+default['haproxy']['config']['defaults']['options'] << 'x_forwarded_for'
+default['haproxy']['config']['defaults']['timeout'] = {
+  'client' => '20s',
+  'server' => '20s',
+  'connect' => '20s'
+}

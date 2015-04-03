30bd6cd5d02c0950535ba8ba324593b73ffdcd78
Richard Shade <rshade@rightscale.com>
2015-04-03 14:56:40 -0500

    adding new version

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'lopaka@rightscale.com'
 license          'All rights reserved'
 description      'client-rs-haproxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
-depends 'rs-haproxy', '~> 1.1.3'
+depends 'rs-haproxy', '~> 1.1.4'
 
 recipe 'client-rs-haproxy::default', 'Runs rs-haproxy::default with custom changes to config.'

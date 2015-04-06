7aa6c5103d782a2e34b22818899c0c1a47d1431b
Richard Shade <rshade@rightscale.com>
2015-04-06 14:01:08 -0500

    trying force_override

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['haproxy']['config']['defaults']['options'] << 'x_forwarded_for'
-default['haproxy']['config']['defaults']['timeout'] = {
+force_override['haproxy']['config']['defaults']['options'] << 'x_forwarded_for'
+force_override['haproxy']['config']['defaults']['timeout'] = {
   'client' => '20s',
   'server' => '20s',
   'connect' => '20s'

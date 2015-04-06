a16559a98b629efc706bd889fe428253f7cf275b
Richard Shade <rshade@rightscale.com>
2015-04-06 14:18:51 -0500

    fixing option array

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-force_override['haproxy']['config']['defaults']['options'] << 'x_forwarded_for'
+force_override['haproxy']['config']['defaults']['option'] <<= 'x_forwarded_for'
 force_override['haproxy']['config']['defaults']['timeout'] = {
   'client' => '20s',
   'server' => '20s',

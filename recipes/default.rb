b0398f17fcf11b8d26e4010d2d70b8ef3bf4f8c1
Richard Shade <rshade@rightscale.com>
2015-04-06 17:34:05 -0500

    removing _ from forward_for

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,6 @@ marker "recipe_start_rightscale" do
   template "rightscale_audit_entry.erb"
 end
 
-node.force_default['haproxy']['config']['defaults']['option'] = [ 'forward_for' ]
+node.force_default['haproxy']['config']['defaults']['option'] = [ 'forwardfor' ]
 
 include_recipe 'rs-haproxy::default'

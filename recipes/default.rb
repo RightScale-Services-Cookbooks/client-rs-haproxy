782425310306dc497630eff2ea77e00175165add
Richard Shade <rshade@rightscale.com>
2015-04-06 17:17:23 -0500

    adding force

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,6 @@ marker "recipe_start_rightscale" do
   template "rightscale_audit_entry.erb"
 end
 
-node.default['haproxy']['config']['defaults']['option'] = [ 'forward_for' ]
+node.force_default['haproxy']['config']['defaults']['option'] = [ 'forward_for' ]
 
 include_recipe 'rs-haproxy::default'
